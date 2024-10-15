e7a203380d9ae84de1855ed858df1d8dd37fd47c
Asus <Asus@192.168.1.224>
2024-10-15 14:28:53 +0530

    Commit

---
+++ DAY14/src/Package1/MyRunnable.java
@@ -0,0 +1,30 @@
+package Package1;
+
+public class MyRunnable implements Runnable 
+{
+
+	@Override
+	public void run() 
+	{
+		for(int i=0;i<5;i++)
+		{
+			System.out.println(i+"\t" + Thread.currentThread().getName());
+		}
+		System.out.println("Done");
+	}
+	public static void main(String[] args) 
+	{
+	
+		Runnable r1 = new MyRunnable();
+		
+		Thread t1 = new Thread(r1);
+		Thread t2 = new Thread(r1);
+		
+		t1.setName("Ping");
+		t2.setName("Pong");
+		
+		t1.start();
+		t2.start();
+	}
+
+}
